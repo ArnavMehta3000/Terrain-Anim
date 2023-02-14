437a6a8f5fde08bd731ddb2b1c9cfee80550b87e
Arnav Mehta <77837038+ArnavMehta3000@users.noreply.github.com>
2023-02-14 15:18:30 +0000

    Added base prject files

---
+++ TerrainAnim/TerrainAnim/pch.h
@@ -0,0 +1,36 @@
+#pragma once
+
+// Precompiled header file for the project.
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <iostream>
+#include <tchar.h>
+#include <memory>
+
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <map>
+#include <unordered_map>
+
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <d3dcompiler.h>
+#include <d3d11_1.h>
+#include <wrl.h>
+
+// Using DXTK
+//#include  <SimpleMath.h>
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "D3DCompiler.lib")
+
+using namespace DirectX;
+
+template <typename T>
+using ComPtr = Microsoft::WRL::ComPtr<T>;
